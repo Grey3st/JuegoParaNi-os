806a4c63993160893e0561a599cc834a6126e05c
Acosta Ledesma Martín Sebastián <martinacosta190@gmail.com>
2023-11-05 16:38:28 -0300

    Se eliminó felicitaciones2.js para que solo felicitaciones.js muestre lo necesario

--- src/components/Felicitaciones.js
+++ src/components/Felicitaciones.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from "./inicio.module.css"
-
-function Felicitaciones({ nombreJugador, puntaje }) {
+function Felicitaciones({ nombreJugador, puntaje, nombreJugador2, puntaje2 }) {
     return (
         <div className= {styles.btn}>
-            <h1>¡Congratulations {nombreJugador}!</h1>
-            <p>Your score is: {puntaje}</p>
+            <h1> ¡Good job, {nombreJugador2}!</h1>
+            <p>Your score is : {puntaje2}</p>
+            <h1> ¡Good job, {nombreJugador}!</h1>
+            <p>Your score is : {puntaje}</p>
         </div>
     );
 }
