1e81127d96b9010de4d0dad8158cebde23ae038f
ElGuasi <matiaseaviles@gmail.com>
2023-11-06 23:43:23 -0300

    Sonidos
    sonidos

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -2,88 +2,111 @@ import React, { useState, useEffect } from 'react';
 import dataAnimal from '../data/dataAnimal.json';
 import styles from "./inicio.module.css";
 
-function Juego({ nombreJugador, puntaje, setPuntaje, alTerminar, rondaActual,setRondaActual }) {
-    const [animalObjetivo, setAnimalObjetivo] = useState('');
-    const [opciones, setOpciones] = useState([]);
-    const [esCorrecto, setEsCorrecto] = useState(null);
-    const [rondasTotales, setRondasTotales] = useState(Math.floor(Math.random() * 6) + 5);
-    const [puedeHacerClic, setPuedeHacerClic] = useState(true);
-  
+function Juego({ nombreJugador, puntaje, setPuntaje, alTerminar, rondaActual, setRondaActual }) {
+  const [animalObjetivo, setAnimalObjetivo] = useState({});
+  const [opciones, setOpciones] = useState([]);
+  const [esCorrecto, setEsCorrecto] = useState(null);
+  const [rondasTotales, setRondasTotales] = useState(Math.floor(Math.random() * 6) + 5);
+  const [puedeHacerClic, setPuedeHacerClic] = useState(true);
+  const [audioContext, setAudioContext] = useState(null);
+  const [audioBuffer, setAudioBuffer] = useState(null);
 
-    const obtenerAnimalAleatorio = () => {
-        const indiceAleatorio = Math.floor(Math.random() * dataAnimal.length);
+  const loadAudio = async (audioUrl) => {
+    const response = await fetch(audioUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    setAudioContext(audioContext);
+    setAudioBuffer(audioBuffer);
+  };
 
-        const arrayNombres = dataAnimal.map(objeto => objeto.nombre);
-        return arrayNombres[indiceAleatorio];
-    };
+  const playSound = () => {
+    if (animalObjetivo && animalObjetivo.sonido && audioContext && audioBuffer) {
+      const source = audioContext.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(audioContext.destination);
+      source.start();
+    }
+  };
 
-    const obtenerOpcionesAleatorias = () => {
-        const animalCorrecto = obtenerAnimalAleatorio();
-        let opcionesAleatorias = [animalCorrecto];
+  const obtenerAnimalAleatorio = () => {
+    const indiceAleatorio = Math.floor(Math.random() * dataAnimal.length);
+    return dataAnimal[indiceAleatorio];
+  };
 
-        while (opcionesAleatorias.length < 3) {
-            const opcion = obtenerAnimalAleatorio();
-            if (!opcionesAleatorias.includes(opcion)) {
-                opcionesAleatorias.push(opcion);
-            }
-        }
+  const obtenerOpcionesAleatorias = () => {
+    const animalCorrecto = obtenerAnimalAleatorio();
+    let opcionesAleatorias = [animalCorrecto];
 
-        opcionesAleatorias = opcionesAleatorias.sort(() => Math.random() - 0.5);
+    while (opcionesAleatorias.length < 3) {
+      const opcion = obtenerAnimalAleatorio();
+      if (!opcionesAleatorias.includes(opcion)) {
+        opcionesAleatorias.push(opcion);
+      }
+    }
 
-        setOpciones(opcionesAleatorias);
-        setAnimalObjetivo(animalCorrecto);
-    };
+    opcionesAleatorias = opcionesAleatorias.sort(() => Math.random() - 0.5);
 
-    const verificarRespuesta = (animalSeleccionado) => {
-        if (animalSeleccionado === animalObjetivo) {
-            setEsCorrecto(true);
-            setPuntaje(puntaje + 1);
-        } else {
-            setEsCorrecto(false);
-        }
-        setPuedeHacerClic(false);
-    
-    };
+    setOpciones(opcionesAleatorias);
+    setAnimalObjetivo(animalCorrecto);
+    loadAudio(animalCorrecto.sonido); // Cargar el audio para el animal correcto
+  };
 
-    const siguienteRonda = () => {
-        if (rondaActual < rondasTotales) {
-            setRondaActual(rondaActual + 1);
-            setEsCorrecto(null);
-            setPuedeHacerClic(true);
-            obtenerOpcionesAleatorias();
-        } else {
-            alTerminar(puntaje);
-        }
-    };
+  const verificarRespuesta = (animalSeleccionado) => {
+    if (animalSeleccionado === animalObjetivo) {
+      setEsCorrecto(true);
+      playSound();
+      setPuntaje(puntaje + 1);
+    } else {
+      setEsCorrecto(false);
+    }
+    setPuedeHacerClic(false);
+  };
 
-    const opcionesDeshabilitadas = esCorrecto !== null;
+  const siguienteRonda = () => {
+    if (rondaActual < rondasTotales) {
+      setRondaActual(rondaActual + 1);
+      setEsCorrecto(null);
+      setPuedeHacerClic(true);
+      obtenerOpcionesAleatorias();
+    } else {
+      alTerminar(puntaje);
+    }
+  };
 
-    useEffect(() => {
-        obtenerOpcionesAleatorias();
-    }, []);
+  const opcionesDeshabilitadas = esCorrecto !== null;
 
-    return (
-        <div className= {styles.btn}>
-            <h1>{nombreJugador}, what is this animal?</h1>
-            <p className= {styles.game}>Game round: {rondaActual}</p>
-            <img src={`img/${animalObjetivo}.png`} alt={animalObjetivo} />
-            <div>
-                {opciones.map((animal) => (
-                    <button
-                        key={animal}
-                        onClick={() => verificarRespuesta(animal)}
-                        disabled={!puedeHacerClic || opcionesDeshabilitadas}
-                    >
-                        {animal}
-                    </button>
-                ))}
-            </div>
-            {esCorrecto === true && <p className={styles.true}>¡Correct!</p>}
-            {esCorrecto === false && <p className= {styles.false}>¡Incorrect!</p>}
-            <button onClick={siguienteRonda}>Next</button>
-        </div>
-    );
+  useEffect(() => {
+    obtenerOpcionesAleatorias();
+  }, []);
+
+  useEffect(() => {
+    if (audioContext) {
+      audioContext.resume();
+    }
+  }, [audioContext]);
+
+  return (
+    <div className={styles.btn}>
+      <h1>{nombreJugador} what is this animal?</h1>
+      <p className={styles.game}>Game round: {rondaActual}</p>
+      <img src={animalObjetivo.imagen} alt={animalObjetivo.nombre} />
+      <div>
+        {opciones.map((animal) => (
+          <button
+            key={animal.nombre}
+            onClick={() => verificarRespuesta(animal)}
+            disabled={!puedeHacerClic || opcionesDeshabilitadas}
+          >
+            {animal.nombre}
+          </button>
+        ))}
+      </div>
+      {esCorrecto === true && <p className={styles.true}>¡Correct!</p>}
+      {esCorrecto === false && <p className={styles.false}>¡Incorrect!</p>}
+      <button onClick={siguienteRonda}>Next</button>
+    </div>
+  );
 }
 
 export default Juego;
-
